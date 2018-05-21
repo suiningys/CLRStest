ed6aa3c5916388e3f0a83a67a177f65a770f017f
suiningys <suiningys@126.com>
2018-05-21 22:32:11 +0800

    first project

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "sortingAlgorithm.h"
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
---
+++ sortingAlgorithm.h
@@ -0,0 +1,12 @@
+#ifndef SORTINGALGORITHM_H_INCLUDED
+#define SORTINGALGORITHM_H_INCLUDED
+#include<vector>
+using std::vector;
+
+void inserSort(vector<double> A)
+{
+
+}
+
+
+#endif // SORTINGALGORITHM_H_INCLUDED
